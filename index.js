b5706e2d1129a7aa5b2f690cc26f59d963ba4687
Barbara <barbara@git>
2023-01-20 11:46:26 +0100

    add colleagues

---
+++ index.js
@@ -0,0 +1,15 @@
+const chalk = require('chalk');
+
+console.log(chalk.blue('Hello world!')); 
+
+console.log(chalk.red('I presents to you 4 of my colleague')); 
+
+console.log(chalk.yellow('Hicham')); 
+
+console.log(chalk.green('Mevine')); 
+
+console.log(chalk.magenta('Vaitu')); 
+
+console.log(chalk.cyan('Mohamed')); 
+
+console.log(chalk.white('Paul')); 
